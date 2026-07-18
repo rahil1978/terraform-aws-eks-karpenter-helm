0cb82750aa638e6d31c368b32f2c5ea1111bb01d
Raheel Aslam <aslamr@fineos.com>
2026-07-18 18:59:28 +1000

    INF-0001 - refactor folder structure

---
+++ projects/infra/variables.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "environment_name" {
+  description = "Environment name used in resource names and tags"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "tags" {
+  description = "Global tags to apply to all resources"
+  type        = map(string)
+  default     = {
+    Terraform = "true"
+  }
+}
+
+variable "subnet_newbits" {
+  description = "Number of new bits to add to VPC CIDR to generate subnets (e.g., 8 means /24 from /16)"
+  type        = number
+  default     = 8
+}
+
+// Customer specific inform
+variable "customer" {
+  description = "Customer name used in resource names and tags"
+  type        = string
+  default     = "folio"
+}
+
+
+variable "customer_prefix" {
+  description = "Customerprefix used in resource names and tags"
+  type        = string
+  default     = "foliodev"
+}
